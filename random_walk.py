4857492cfb2f6edc96c436149ee8b6d50572545c
vlastroia <vlad.stroia@gmail.com>
2019-10-29 21:25:17 +0200

    makes 1000 page pdf with random 100000 steps random walks

--- random_walk.py
+++ random_walk.py
@@ -1,12 +1,15 @@
 from matplotlib import pyplot as plt
 import random
-max = 100000
-x = [i for i in range(max)]
+from matplotlib.backends.backend_pdf import PdfPages
+max = 100000 #number of random walk steps
+pages = 1000 #number of random walks to be made
+with PdfPages("rezultat.pdf") as pdf:
+    for page in range(pages): 
+        x = [i for i in range(max)] #time axis
+        y = [0] #position axis
+        for i in range(1, max): 
+            y.append(y[i-1]  + random.randint(-1,1)) #go one step up, one step down or stay still  
 
-y = [0]
-
-for i in range(1, max):
-    y.append(y[i-1]  + random.randint(-1,1)) 
-
-plt.plot(x, y)
-plt.show()
+        plt.plot(x, y)
+        pdf.savefig()  # saves the current figure into a pdf page
+        plt.close()
